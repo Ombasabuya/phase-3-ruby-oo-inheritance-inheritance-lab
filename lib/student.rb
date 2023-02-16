0cef9379d9e9c4a263edc60f4335bcd845278b22
ombasaabuya <ombasabuya@gmail.com>
2023-02-16 21:55:22 +0300

    completed lib/student.rb

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
-
-end
+require_relative './user'
+    class Student < User
+    
+      attr_accessor :knowledge
+    
+      def initialize
+        @knowledge = []
+      end
+    
+      def learn(ruby_knowledge)
+        @knowledge << ruby_knowledge
+      end
+    
+      def knowledge
+        @knowledge
+      end
+    
+    end
